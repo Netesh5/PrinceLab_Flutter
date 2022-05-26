7b3d834522b5d40c68c2143d8152b75562def321
Netesh5 <netesh.paudel@gmail.com>
2022-05-26 22:36:01 +0545

    HomePage creation

--- paradox/lib/main.dart
+++ paradox/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:paradox/Screens/homepage.dart';
 
 void main() {
   runApp(myApp());
 }
 
+//Main class where Homepage is called
 class myApp extends StatelessWidget {
   const myApp({Key? key}) : super(key: key);
 
@@ -11,6 +13,7 @@ class myApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: Homepage(),
     );
   }
 }
